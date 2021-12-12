34cdb0aa44413836bb0d346f5f6a4834e880768f
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2021-12-12 11:30:19 +0100

    Initial server setup

---
+++ server/src/main/java/be/mbict/server/TimeLimitController.java
@@ -0,0 +1,21 @@
+package be.mbict.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+@RestController
+public class TimeLimitController {
+
+    @GetMapping("/timelimit")
+    public List<Book> allBooks(@RequestParam(value = "wait", defaultValue = "0", required = false) long millis) throws InterruptedException {
+        MILLISECONDS.sleep(millis);
+        return List.of(new Book("Stephen Hawking", "A Brief History of Time"), new Book("Douglas Adams", "The Hitchhiker's Guide to the Galaxy"));
+    }
+}
+
+record Book(String author, String title) {}
